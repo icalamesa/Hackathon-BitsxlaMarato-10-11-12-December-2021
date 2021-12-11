6e0c576f5043ba01d72444920d9d6418117ca8aa
Laura Garcia <laura.garcia.l1@estudiantat.upc.edu>
2021-12-11 14:01:13 +0100

    Audios funcionan rec/env

--- chimuelo.py
+++ chimuelo.py
@@ -43,28 +43,62 @@ def photo(update, context):
     name = str(update.effective_chat.id)
     _create_dir("files")
     _create_dir("files/" + name)
+    _create_dir("files/" + name + "/images")
     newFile = context.bot.getFile(update.message.photo[-1].file_id)
     time = datetime.datetime.now().strftime("%d-%m-%y_%H:%M,%S")
-    newFile.download(os.path.join("./files/" + name + "/", time + ".jpg"))
+    newFile.download(os.path.join("./files/" + name + "/images/", time + ".jpg"))
     print("Download succesful")
 
 def send_photo(update, context):
-    path = "./files/" + str(update.effective_chat.id)
-    imgs = os.listdir(path)
-    print(imgs)
-    img = random.choice(imgs)
-    print(img)
-    im = open(os.path.join(path, img), 'rb')
-    context.bot.send_photo(
-        chat_id=update.effective_chat.id,
-        photo = im
-    )
+    try:
+        path = "./files/" + str(update.effective_chat.id) + "/images"
+        imgs = os.listdir(path)
+        print(imgs)
+        img = random.choice(imgs)
+        print(img)
+        im = open(os.path.join(path, img), 'rb')
+        context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo = im
+        )
+    except:
+        context.bot.send_message(
+            chat_id = update.effective_chat.id,
+            text = "Encara no m'has enviat cap foto. Anima't a fer-ho per poder recuperar-ho en el futur!"
+        )
 
 def eco(update, context):
     context.bot.send_message(
         chat_id = update.effective_chat.id,
         text = update.message.text
     )
+
+def audio(update, context):
+    name = str(update.effective_chat.id)
+    _create_dir("files")
+    _create_dir("files/" + name)
+    _create_dir("files/" + name + "/audios")
+    voice = context.bot.getFile(update.message.voice.file_id)
+    time = datetime.datetime.now().strftime("%d-%m-%y_%H:%M,%S")
+    voice.download(os.path.join("./files/" + name + "/audios", time + ".mp3"))
+    print("Download succesful")
+
+def send_audio(update, context):
+    try:
+        path = "./files/" + str(update.effective_chat.id) + "/audios"
+        auds = os.listdir(path)
+        # print(auds)
+        aud = random.choice(auds)
+        print(aud)
+        context.bot.send_audio(
+            chat_id = update.effective_chat.id,
+            audio = open(os.path.join(path, aud), 'rb')
+        )
+    except:
+        context.bot.send_message(
+            chat_id = update.effective_chat.id,
+            text = "Encara no m'has enviat cap audio. Anima't a fer-ho per poder recuperar-ho en el futur!"
+        )
 
 # Declare a constant with token acces read from token.txt
 TOKEN = open('token.txt').read().strip()
@@ -81,10 +115,15 @@ dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('register', register))
 # Indicates that when the bot receives the command /photo, the function help is executed
 dispatcher.add_handler(CommandHandler('photo', send_photo))
+# Indicates that when the bot receives the command /photo, the function help is executed
+dispatcher.add_handler(CommandHandler('audio', send_audio))
 # Indicates that when the bot receives a photo, the function photo is executed
 updater.dispatcher.add_handler(MessageHandler(Filters.photo, photo))
-
+# Indicates that when the bot receives a text, the function eco is executed
 updater.dispatcher.add_handler(MessageHandler(Filters.text, eco))
+# Indicates that when the bot receives an audio, the function audio is executed
+updater.dispatcher.add_handler(MessageHandler(Filters.voice, audio))
+
 
 # Start the bot
 updater.start_polling()
